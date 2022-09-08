f8d61bcc50d33024084b687fa7e7377e4f373632
Denis <gabbas.denn@mail.ru>
2022-09-08 06:14:38 +0300

    practice with useMemo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import {Component, useState, useEffect, useCallback} from 'react';
+import {useState, useEffect, useCallback, useMemo} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
+
+const countTotal = (num) => {
+    console.log('counting...');
+    return num + 10;
+}
+
+
 // class Slider extends Component {
 
 //     constructor(props) {
@@ -103,7 +110,19 @@ const Slider = (props) => {
     // function toggleAutoplay() {
     //     setState(state => ({...state, autoplay: !state.autoplay}));
     // }
+
+    const total = useMemo(() => {
+        return countTotal(slide);
+    }, [slide]);
+
+    const style = useMemo(() => ({
+        color: slide > 4 ? 'red' : 'black'
+    }), [slide])
+    
   
+    useEffect(() => {
+        console.log('styles!')
+    }, [style])
 
     return (
         <Container>
@@ -121,6 +140,7 @@ const Slider = (props) => {
                 <Slide getSomeImages={getSomeImages}/>
 
                 <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null} </div>
+                <div style={style} className="text-center mt-5">Total slides: {total} </div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
