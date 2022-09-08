3e2285e00c9d80efa1a642ce617638c1c1c0b034
Denis <gabbas.denn@mail.ru>
2022-09-08 05:51:23 +0300

    practice with hook useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component, useState, useEffect} from 'react';
+import {Component, useState, useEffect, useCallback} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 // class Slider extends Component {
@@ -55,28 +55,38 @@ import './App.css';
 // }
 
 
+
+
 const Slider = (props) => {
 
     const [slide, setSlide] = useState(0);
     const [autoplay, setAutoplay] = useState(false);
 
-    function logging() {
-        console.log('log!');
-    }
+    // function logging() {
+    //     console.log('log!');
+    // }
 
-    useEffect(() => {
-        console.log("effect");
-        document.title = `Slide: ${slide}`;
+    // useEffect(() => {
+    //     // console.log("effect");
+    //     document.title = `Slide: ${slide}`;
 
-        window.addEventListener('click', logging);
+    //     window.addEventListener('click', logging);
 
-        return () => {
-            window.removeEventListener('click', logging);
-        }
+    //     return () => {
+    //         window.removeEventListener('click', logging);
+    //     }
 
-    }, [slide]);
+    // }, [slide]);
 
     // const [state, setState] = useState({slide: 0, autoplay: false});
+
+    const getSomeImages = useCallback(() => {
+        console.log('fetching')
+        return [
+            'https://avatars.mds.yandex.net/i?id=ba2278c7e2a688a42b00fc6e1bfe46f5-3927965-images-thumbs&ref=rim&n=33&w=281&h=188',
+            'https://gas-kvas.com/uploads/posts/2022-06/1655196624_12-gas-kvas-com-p-tyulen-foto-zhivotnogo-12.jpg'
+        ]
+    }, [slide]);
 
     function changeSlide(i) {
         setSlide(slide => slide + i);
@@ -98,7 +108,18 @@ const Slider = (props) => {
     return (
         <Container>
             <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+              
+                {/* {
+                    getSomeImages().map((url, i) => {
+                        return (
+                            <img className="d-block w-100" src={url}alt="slide" key={i}/>
+  
+                        )
+                    })
+                } */}
+
+                <Slide getSomeImages={getSomeImages}/>
+
                 <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null} </div>
                 <div className="buttons mt-3">
                     <button 
@@ -116,6 +137,20 @@ const Slider = (props) => {
                 </div>
             </div>
         </Container>
+    )
+}
+
+const Slide = ({getSomeImages}) => {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        setImages(getSomeImages())
+    }, [getSomeImages])
+
+    return (
+        <>
+            {images.map((url, i) => <img className="d-block w-100" src={url}alt="slide" key={i}/>)}
+        </>
     )
 }
 
